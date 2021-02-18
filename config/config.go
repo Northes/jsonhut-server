782b45b0a1d820366490bed27795a825c2862f79
North <45125250+Northes@users.noreply.github.com>
2021-02-18 22:44:38 +0800

    优化配置项以及配置文件

--- config/config.go
+++ config/config.go
@@ -1,17 +1,18 @@
 package config
 
 import (
-	"fmt"
 	"gopkg.in/ini.v1"
-	"os"
 )
 
 var App appStruct
 
 type appStruct struct {
-	AppName string `ini:"app_name"`
-	Port    string `ini:"app_port"`
-	BaseUrl string `ini:"base_url"`
+	AppName               string `ini:"app_name"`
+	Port                  string `ini:"app_port"`
+	BaseUrl               string `ini:"base_url"`
+	PostIPCurrentLimit    int    `ini:"post_ip_current_limit"`
+	GetIPCurrentLimit     int    `ini:"get_ip_current_limit"`
+	DetailsIPCurrentLimit int    `ini:"details_ip_current_limit"`
 }
 
 type configStruct struct {
@@ -21,23 +22,26 @@ type configStruct struct {
 }
 
 func LoadConfig() {
-	cfgDefault, err := ini.Load(".env")
+	//cfgDefault, err := ini.Load(".env")
+	//if err != nil {
+	//	fmt.Printf("Fail to read file: %v", err)
+	//	os.Exit(1)
+	//}
+	//
+	//var cfg *ini.File
+	//if cfgDefault.Section("").Key("app_mode").String() == "development" {
+	//	cfg, err = ini.Load(".env.dev")
+	//} else {
+	//	cfg, err = ini.Load(".env.prod")
+	//}
+	//if err != nil {
+	//	fmt.Printf("Fail to read file: %v", err)
+	//	os.Exit(1)
+	//}
+	cfg, err := ini.LooseLoad(".env", ".env.local")
 	if err != nil {
-		fmt.Printf("Fail to read file: %v", err)
-		os.Exit(1)
+		panic(err)
 	}
-
-	var cfg *ini.File
-	if cfgDefault.Section("").Key("app_mode").String() == "development" {
-		cfg, err = ini.Load(".env.dev")
-	} else {
-		cfg, err = ini.Load(".env.prod")
-	}
-	if err != nil {
-		fmt.Printf("Fail to read file: %v", err)
-		os.Exit(1)
-	}
-
 	config := new(configStruct)
 
 	err = cfg.MapTo(config)
